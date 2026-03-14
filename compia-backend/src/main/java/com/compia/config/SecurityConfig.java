6db5a6cb3dc85ecbc58243bf9be0ac8f5b92a7e9
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2026-03-13 22:36:52 -0300

    fix cors

--- compia-backend/src/main/java/com/compia/config/SecurityConfig.java
+++ compia-backend/src/main/java/com/compia/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.compia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,8 +14,10 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {}) //
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll()
                 );
 
