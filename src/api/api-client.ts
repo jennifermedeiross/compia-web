715462f03addda78f348d38b7248e62a7e149336
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2026-03-13 22:22:23 -0300

    env do front

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8081/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function apiFetch(path: string, options?: RequestInit) {
   const res = await fetch(`${BASE_URL}${path}`, {
